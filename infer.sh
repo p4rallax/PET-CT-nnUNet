e306030ac32c0010f59104c7ef6d7fd7c0fdde13
Satwik <satwik929@gmail.com>
2024-12-05 14:00:59 +1030

    Add files via upload

--- infer.sh
+++ infer.sh
@@ -3,13 +3,16 @@
 # Input arguments
 input_file1="$1"
 input_file2="$2"
-output_folder="$3"
+
+output_file="$3"
 renamed_file1="image_0000.nii.gz"  # Adjust to the expected name
 renamed_file2="image_0001.nii.gz"  # Adjust to the expected name
 
 # Copy and rename files to the expected locations
-cp "/data/input/$input_file1" "/nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs/$renamed_file1"
-cp "/data/input/$input_file2" "/nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs/$renamed_file2"
+cp "/data/$input_file1" "/nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs/$renamed_file1"
+cp "/data/$input_file2" "/nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs/$renamed_file2"
+
+mkdir /data/nnunet_out
 
 export nnUNet_raw="/nnUNet_raw_data_base"
 export nnUNet_preprocessed="/nnUNet_preprocessed"
@@ -17,4 +20,7 @@ export nnUNet_results="/nnUNet_trained_models"
 
 # Run nnUNet inference (adjust command as needed)
 # python /infer.py -o $output_folder
-nnUNetv2_predict -d Dataset703_FS_SCGH_Train -f 0 1 2 3 4 -tr nnUNetTrainer -c 3d_fullres_resenc -p nnUNetPlans -i /nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs -o $output_folder
+nnUNetv2_predict -d Dataset703_FS_SCGH_Train -f 0 1 2 3 4 -tr nnUNetTrainer -c 3d_fullres_resenc -p nnUNetPlans -i /nnUNet_raw_data_base/Dataset703_FS_SCGH_Train/imagesTs -o /data/nnunet_out/
+
+nii_file=$(find "/data/nnunet_out" -type f -name "*.nii.gz")
+cp "$nii_file" "/data/output/$output_file"
